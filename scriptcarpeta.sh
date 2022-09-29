fcaf32ff0827278cdb9eaa691023f1899bdf8d95
baphomet <114076661+baphometG@users.noreply.github.com>
2022-09-29 16:46:04 +0200

    Delete scriptcarpeta.sh

--- scriptcarpeta.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir Mi_carpeta_cifrada_105 Mi_carpeta_no_cifrada_105
-cd ./Mi_carpeta_cifrada_105
-cat ../textolibro.txt | tee Fich_105_1 Fich_105_2
-
-cd ..
-cd ./Mi_carpeta_no_cifrada_105
-cat ../textolibro.txt | tee  Fich_105_3 Fich_105_4
